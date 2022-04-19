f8fa95c3ee2bbe9ca624084d744aa07d79908b25
SnehaTagadiya <snehapatel27996@gmail.com>
2022-04-19 15:14:35 +0530

    Assignment

--- src/Component/PostDetail.js
+++ src/Component/PostDetail.js
@@ -1,43 +1,51 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React,{useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 function PostDetail() {
-  const [data, setData] = useState([]);
+    const [data, setData]=useState([]);
 
-  useEffect(() => {
-    showData();
-  }, []);
+    useEffect(()=>{
+        showData()
+    },[]);
 
-  const showData = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => response.json())
-      .then((json) => console.log(json));
-  };
-
+    const showData = () => {
+       axios.get('https://jsonplaceholder.typicode.com/posts/1')
+       .then((response) => {
+        console.log(response);
+        setData(response.data);
+      });
+    
+    };
+    
   return (
     <div>
-      <h1>Post Details</h1>
-      <table border="3" className="table">
-        <thead className="thead-dark">
-          <tr>
-            <th>Id</th>
-            <th>Title</th>
-            <th>Body</th>
-          </tr>
-        </thead>
+        <h1>Post Details</h1>
+        <table border="3" className='table'>
+            <thead className='thead-dark'>
+                <tr>
 
-        <tbody>
-          {data.map((user, i) => (
-            <tr key={i}>
-              <td>{user.id}</td>
-              <td>{user.title}</td>
-              <td>{user.body}</td>
+                <th>Id</th>
+                <th>Title</th>
+                <th>Body</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            </thead>
+
+            <tbody>
+                {
+               data.map((user,i)=>(
+                   <tr key={i}>
+                       <td>{user.id}</td>
+                       <td>{user.title}</td>
+                       <td>{user.body}</td>
+                   </tr>
+               ))
+               }
+            </tbody>
+        </table>
+
     </div>
-  );
+  )
 }
 
-export default PostDetail;
+export default PostDetail
